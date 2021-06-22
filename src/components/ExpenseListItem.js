b389466a437054ca33c4e2d6a04f6427a93bc278
Samir Muratovic <ssamirmuratovic@gmail.com>
2021-06-22 09:16:11 -0500

    fix amount decimal formatting

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
+import numeral from 'numeral'
 
 const ExpenseListItem = ({ id, description, amount, note, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount}</p>
-    <p>{note}</p>
-    <p>{moment(createdAt).calendar()}</p>
+    <p>
+      {numeral(amount / 100).format('$0,0.00')} -
+      {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
   </div>
 )
 
