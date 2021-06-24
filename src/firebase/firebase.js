a1ed45b62310114aaa9f67aef7c4ae9d251a2964
Samir Muratovic <ssamirmuratovic@gmail.com>
2021-06-24 17:02:43 -0500

    add environment variables for database access

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBbqepj4R9SYpDsRQn7Zx51HFKJ1GPAV5A',
-  authDomain: 'budget-14fc4.firebaseapp.com',
-  databaseURL: 'https://budget-14fc4-default-rtdb.firebaseio.com',
-  projectId: 'budget-14fc4',
-  storageBucket: 'budget-14fc4.appspot.com',
-  messagingSenderId: '1059454197626',
-  appId: '1:1059454197626:web:28ec5d868a58edf89340c3',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
