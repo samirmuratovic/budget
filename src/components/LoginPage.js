6ffd01cd5d5056c6388b2d1d4875b980c9cc569e
Samir Muratovic <ssamirmuratovic@gmail.com>
2021-06-25 09:51:15 -0500

    add login page skeleton

---
+++ src/components/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default LoginPage
