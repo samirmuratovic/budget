77f80df6750963dcb7af8645560caae5d1585835
Samir Muratovic <ssamirmuratovic@gmail.com>
2021-06-25 10:14:45 -0500

    implement log out functionality with firebase

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { startLogout } from '../actions/auth'
 
-const Header = () => (
-  <header>
-    <h1>Budget App</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
-      Dashboard
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active">
-      Add Expense
-    </NavLink>
-  </header>
-)
+const Header = () => {
+  const dispatch = useDispatch()
+  return (
+    <header>
+      <h1>Budget App</h1>
+      <NavLink to="/" activeClassName="is-active" exact={true}>
+        Dashboard
+      </NavLink>
+      <NavLink to="/create" activeClassName="is-active">
+        Add Expense
+      </NavLink>
+      <button onClick={() => dispatch(startLogout())}>Logout</button>
+    </header>
+  )
+}
 
 export default Header
