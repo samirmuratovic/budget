ce8f28d297949dedf313b6cfb3633a994e9ca9c0
Samir Muratovic <ssamirmuratovic@gmail.com>
2021-06-25 20:06:42 -0500

    add styles for expense list items

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { login, logout } from './actions/auth'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
 import { firebase } from './firebase/firebase'
+import LoadingPage from './components/LoadingPage'
 
 const store = configureStore()
 
@@ -25,7 +26,7 @@ const renderApp = () => {
   }
 }
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('root'))
+ReactDOM.render(<LoadingPage />, document.getElementById('root'))
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
